64a3b45f17dd56d5c1b5f1e662e361f9f65e316e
Cristian2023220 <cristiansouto2019@gmail.com>
2023-12-18 23:41:40 -0300

    Questão 5-Algoritimos de Decisão

---
+++ Questao5/src/questao5/Questao5.java
@@ -0,0 +1,16 @@
+package questao5;
+import javax.swing.JOptionPane;
+
+public class Questao5 {
+
+
+    public static void main(String[] args) {
+
+        String estrada = JOptionPane.showInputDialog("Digite a distância percorrida em km ");
+        double distancia = Double.parseDouble (estrada);
+        estrada = JOptionPane.showInputDialog (Digite a quantidade de combustível gasto em litros);
+        
+        
+    }
+    
+}
